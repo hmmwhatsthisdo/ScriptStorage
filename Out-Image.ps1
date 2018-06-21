094a307d2d4b436074f15f316877a2060581740e
Sean Williams <hmmwhatsthisdo@gmail.com>
2018-06-21 15:25:13 -0700

    Add Out-Image and Unprotect-SecureString

---
+++ Out-Image.ps1
@@ -0,0 +1,50 @@
+﻿Function Out-Image {
+Param(
+
+    [Parameter(
+        Mandatory = $true,
+        Position = 0,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true
+    )]
+    [ValidateNotNullOrEmpty()]
+    [System.Drawing.Image]$Image,
+
+    [Parameter(
+        Position = 1
+    )]
+    [String]$Title = "Image Viewer"
+
+
+)
+
+Begin {
+
+}
+
+Process {
+    
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+
+    $Form = [System.Windows.Forms.Form]::new()
+
+    $Form.Text = $Title
+    $Form.Height = $Image.Size.Height
+    $Form.Width = $Image.Size.Width
+    
+    $PictureBox = [System.Windows.Forms.PictureBox]::new()
+    $PictureBox.Height = $Image.Size.Height
+    $PictureBox.Width = $Image.Size.Width
+    $PictureBox.SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::CenterImage
+    
+    $PictureBox.Image = $Image
+
+    $Form.Controls.Add($PictureBox)
+
+    $Form.Add_Shown( { $Form.Activate() } )
+    
+	$Form.ShowDialog() | Out-Null
+
+}
+
+}
---
+++ Unprotect-SecureString.ps1
@@ -0,0 +1,30 @@
+﻿function Unprotect-SecureString {
+[CmdletBinding()]
+Param(
+    [parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true
+    )]
+    [Alias(
+        "Password",
+        "SS"
+    )]
+    [ValidateNotNull()]
+    [SecureString[]]$SecureString
+)
+
+    Process {
+        $SecureString | ForEach-Object {
+            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($_)
+            
+            try {
+                return [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($BSTR)
+            } finally {
+                [System.Runtime.InteropServices.Marshal]::FreeBSTR($BSTR)
+            }
+            
+        }
+    }
+
+}
